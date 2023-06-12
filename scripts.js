d9876dd07a55211ff086efbb1b8d41bfd0e45b30
kirstenm99 <devkirstenm@gmail.com>
2023-06-12 16:10:40 +0200

    Updated styling

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Get the elements
+// Selects elements
 const counter = document.querySelector('.custom-badge');
 const plusButton = document.querySelector('.buttons-wrapper sl-button:nth-child(2)');
 const minusButton = document.querySelector('.buttons-wrapper sl-button:nth-child(1)');
@@ -8,19 +8,19 @@ const resetButton = document.querySelector('sl-button[variant="neutral"]');
 let count = 0;
 counter.textContent = count;
 
-// Add event listeners
+// Adds event listeners to each button
 plusButton.addEventListener('click', incrementCounter);
 minusButton.addEventListener('click', decrementCounter);
 resetButton.addEventListener('click', resetCounter);
 
-// Functions
+// Button functions
 function incrementCounter() {
-  count++;
+  count++; // shorthand notation for 'count = count + 1'
   counter.textContent = count;
 }
 
 function decrementCounter() {
-  if (count > 0) {
+  if (count > 0) { // only decrements if count is more than 0 (therefore if it's 1 or more)
     count--;
     counter.textContent = count;
   }
@@ -29,5 +29,5 @@ function decrementCounter() {
 function resetCounter() {
   count = 0;
   counter.textContent = count;
-  alert('The counter has been reset.');
+  alert('The counter has been reset.'); // displays confirmation message that counter has been reset
 }
